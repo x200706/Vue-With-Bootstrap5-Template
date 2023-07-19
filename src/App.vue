bb613e2c2156d9769678d1f24122badbebe56eec
chi200706 <19492931-chi200706@users.noreply.replit.com>
2023-07-19 09:33:04 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <HelloWorld /> 
+    </div>
+  </header>
+
+  <main>
+   
+  </main>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<script setup>
+</script>
+
+<template>
+<nav class="navbar navbar-dark bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+    </div>
+</nav>
+
+<div class="container">
+  <div class="row">
+    <div class="col">
+      Column
+    </div>
+    <div class="col">
+      Column
+    </div>
+    <div class="col">
+      Column
+    </div>
+  </div>
+</div>
+</template>
+
+<style scoped>
+
+</style>
